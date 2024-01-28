89b155ab7b6d379f0df9132e3588c8de7733f886
Rohit Sinha <rohitslg.003@gmail.com>
2024-01-28 05:54:16 +0530

    fullstack medical application made

---
+++ MedicalApplication/app/src/main/java/com/example/myapplication/OrderDetViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDetViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_det_view);
+    }
+}
---
+++ MedicalApplication/app/src/main/java/com/example/myapplication/Test_CheckActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test_CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_check);
+    }
+}
